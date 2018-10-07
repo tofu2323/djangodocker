3b7c2c81f8da1d28a3f1e750d6ac07536082ca65
gotch <gotch@styz.io>
2018-10-07 18:05:49 +0900

    dockerized. https://blog.devartis.com/django-development-with-docker-a-step-by-step-guide-525c0d08291

--- djangodocker/settings.py
+++ djangodocker/settings.py
@@ -14,7 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-ROOT_DIR = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -77,7 +76,7 @@ DATABASES = {
         'NAME': 'djangodocker_db',
         'USER': 'root',
         'PASSWORD': 'root',
-        'HOST': '127.0.0.1',  # Or an IP Address that your DB is hosted on
+        'HOST': 'db',  # Cambiamos por el nombre del servicio
         'PORT': '3306',
     }
 }
@@ -117,5 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(ROOT_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
